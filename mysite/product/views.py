b125427a37548152109639850f3dd26adf107ae5
Alexxxium <alexium@mail.ru>
2023-05-11 17:54:26 +0300

    Chapter 5

--- mysite/product/views.py
+++ mysite/product/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+# from matplotlib import context
+from .models import Product_type
+from django.core.paginator import Paginator
 
 
 def product(request):
-    return render(request, 'product/index.html')
+    
+    product_types = Product_type.objects.all()
+    paginator = Paginator(product_types, 3)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+    
+    prev = '?page={}'.format(page.previous_page_number()) if page.has_previous() else ''
+    next = '?page={}'.format(page.next_page_number()) if page.has_next() else ''
+
+    context = {
+        'product_types': page,
+        'is_paginated': is_paginated,
+        'prev_url': prev,
+        'next_url': next,
+    }
+
+    return render(request, 'product/index.html', context=context)
