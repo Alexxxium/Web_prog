8c058978968823977226ebbcf3b7093b7734e6db
Alexxxium <alexium@mail.ru>
2023-05-19 16:45:01 +0300

    Chapter 6

--- mysite/product/views.py
+++ mysite/product/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-# from matplotlib import context
+
 from .models import Product_type
+from django import forms
+
 from django.core.paginator import Paginator
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+from django.contrib import messages
+from django.urls import reverse_lazy 
 
 
 def product(request):
     
-    product_types = Product_type.objects.all()
+    product_types = Product_type.objects.order_by('-id')
     paginator = Paginator(product_types, 3)
 
     page_number = request.GET.get('page', 1)
@@ -24,3 +30,40 @@ def product(request):
     }
 
     return render(request, 'product/index.html', context=context)
+
+
+class Product_typeForm(forms.ModelForm):
+    class Meta:
+        model = Product_type
+        fields = ['title']
+        lables = {
+            'title': 'Название типа продукта',
+        }
+        widgets = {
+            'title': forms.TimeInput(attrs = {'class': 'form-control'}),
+        }
+
+
+class Product_typeCreate(CreateView):
+    model = Product_type
+    form_class = Product_typeForm
+    template_name = 'product/create_product_type.html'
+    success_url = '/product/'
+
+
+class Product_typeUpdate(UpdateView):
+    model = Product_type
+    form_class = Product_typeForm
+    template_name = 'product/update_product_type.html'
+    success_url = '/product/'
+
+
+class Product_typeDelete(DeleteView):
+    model = Product_type
+    context_object_name = 'product'
+    success_url = reverse_lazy('product_url')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted siccessfully")
+        return super(Product_typeDelete, self).form_valid(form)
+    
